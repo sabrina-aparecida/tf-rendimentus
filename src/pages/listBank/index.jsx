df42542cdb6a4c41572336fe29786338afda08f0
Mariana Barros <mamirandabarros11@gmail.com>
2020-09-09 20:00:55 -0300

    alteraçao de utilizaçao de api de  axios pra fetch

--- src/pages/listBank/index.jsx
+++ src/pages/listBank/index.jsx
@@ -1,23 +1,26 @@
 import React, { useState, useEffect } from 'react';
 import ItemBank from '../../components/itemBank'
 import './style.css'
-import api from "../../service/api";
 
 
 function ListBank() {
   const [banks, setBanks] = useState([])
 
   useEffect(() => {
-    api
-      .get("banks"
-      )
+    fetch('https://jsonbox.io/box_ddb0ab5da8d69da8c315/banks')
       .then((response) => {
-        setBanks(response.data);
+        return response.json();
+      })
+      .then((data) => {
+        setBanks(data);
+
       })
       .catch((e) => {
         alert("ocorreu um erro ao tentar obter os dados");
       });
   }, []);
+
+
 
   return (
     <>
@@ -25,7 +28,7 @@ function ListBank() {
       <ul>
         {banks.map(item => (<ItemBank numBank={item.bankNumber} nameBank={item.name} saldoAcount={item.saldo} />))}
       </ul>
-      <span className="saldo">SALDO DISPONÍVEL DE TODAS AS CONTAS:   R$   R$: 5.000,00</span>
+      <span className="saldo">SALDO DISPONÍVEL DE TODAS AS CONTAS:   R$: 5.000,00</span>
 
     </>
   )

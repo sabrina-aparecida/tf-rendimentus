d9cdecc6088a5c83cccb13acbe7057db41a13935
Karine Sardela <kfsardela@gmail.com>
2020-09-10 21:25:11 -0300

    Estilização pagina de ofertas

--- src/components/offer_message/index.jsx
+++ src/components/offer_message/index.jsx
@@ -10,25 +10,18 @@ function OfferMessage() {
         return response.json();
       })
       .then((data) => {
-          //Loop do banco
         const sum = data.map(bank => {
 
             return {
-                //Soma de todos saldos das contas no banco
                 sumBalance: bank.accounts.reduce((accumulator, currentValue) => (accumulator + currentValue.balance), 0),
-                // Soma de todas as faturas do banco
                 sumExtract: bank.accounts
-                // Loop de todas as contas
                 .map(account => {
                     return account.accountExtract
-                    // Soma de todos os itens da fatura da conta
                     .reduce((accumulator, currentValue) => (accumulator + currentValue.value), 0)
                 })
-                // Soma todos os faturas do banco
                 .reduce((accumulator, currentValue) => (accumulator + currentValue), 0)
             }
         })
-        // Soma de todos os saldos e faturas de todos os bancos
         .reduce((accumulator, currentValue) => ({
             totalBalance: accumulator.totalBalance + currentValue.sumBalance,
             totalExtract: accumulator.totalExtract + currentValue.sumExtract,
